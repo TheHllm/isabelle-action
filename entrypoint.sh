5fe2b85fca77f398fc8e5fb14cd1ddb5c16b0b52
pandaman64 <kointosudesuyo@infoseek.jp>
2020-09-19 15:18:10 +0900

    copy heap

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
+echo "$*"
+
+# Run Isabelle
 sh -c "/Isabelle2020/bin/isabelle $*"
+
+# Copy Heap
+# This command uses `isabelle env` for populating a new shell with Isabelle environment variables,
+# and 'EOC' flood the shell with the following commands, while preventing the variables from
+# begin substitued by the outer shell (that does not have the environment variables).
+/Isabelle2020/bin/isabelle env bash << 'EOC'
+if [ -d $ISABELLE_HEAPS ]; then
+  cp -r $ISABELLE_HEAPS heaps
+fi
+EOC
