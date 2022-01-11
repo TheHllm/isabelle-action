b6ece883f33ce7762de28c1a9b09339e50bd3356
Brae Webb <email@braewebb.com>
2022-01-11 17:14:52 +1000

    Hopefully pass arguments correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
 ISABELLE_VERSION=$1
+shift
 
 cd /container
 echo "creating docker image with isabelle version: $ISABELLE_VERSION"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t container --build-arg isabelle_version="$ISABELLE_VERSION" . && docker run -e HEAP_CACHE_DIR=${HEAP_CACHE_DIR} container ${@:2}
+docker build -t container --build-arg isabelle_version="$ISABELLE_VERSION" . && docker run -e HEAP_CACHE_DIR=${HEAP_CACHE_DIR} container ${@}
