93ebb519c2a1f0a3476e39252c83d31aa3297428
Brae Webb <email@braewebb.com>
2022-12-16 18:08:54 +1000

    Update to Isabelle2022

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 # This command uses `isabelle env` for populating a new shell with Isabelle environment variables,
 # and 'EOC' flood the shell with the following commands, while preventing the variables from
 # begin substitued by the outer shell (that does not have the environment variables).
-/Isabelle2021-1/bin/isabelle env bash << 'EOC'
+/Isabelle2022/bin/isabelle env bash << 'EOC'
 if [ -n "$HEAP_CACHE_DIR" -a -d "$HEAP_CACHE_DIR" ]; then
   mkdir -p "$ISABELLE_HEAPS"
   cp -r "$HEAP_CACHE_DIR"/* "$ISABELLE_HEAPS"
@@ -14,10 +14,10 @@ fi
 EOC
 
 # Run Isabelle
-sh -c "/Isabelle2021-1/bin/isabelle $*"
+sh -c "/Isabelle2022/bin/isabelle $*"
 
 # Copy Heap Images from Local to Cache
-/Isabelle2021-1/bin/isabelle env bash << 'EOC'
+/Isabelle2022/bin/isabelle env bash << 'EOC'
 if [ -n "$HEAP_CACHE_DIR" -a -d "$ISABELLE_HEAPS" ]; then
   mkdir -p "$HEAP_CACHE_DIR"
   cp -r "$ISABELLE_HEAPS"/* "$HEAP_CACHE_DIR"
